3c016b03b586959d327642f4b21643cf54fd2e37
David Yu <keweihk@gmail.com>
2011-10-13 14:53:25 +0800

    Add a func that updata touchscreen's status
    Add a func that updata touchscreen's status
    
    Signed-off-by: David Yu <keweihk@gmail.com>
    Signed-off-by: Michael.Kang <blackfin.kang@gmail.com>

--- device/touchscreen_s3c6410/touchscreen_s3c6410.c
+++ device/touchscreen_s3c6410/touchscreen_s3c6410.c
@@ -18,51 +18,127 @@
 #include <skyeye_class.h>
 #include <skyeye_interface.h>
 #include <skyeye_obj.h>
-#include <skyeye_mm.h>
+#include <skyeye_mm.h> 
 #include <memory_space.h>
+#include <skyeye_device.h>
 #define DEBUG
 #include <skyeye_log.h>
 
 #include "touchscreen_s3c6410.h"
 
-static exception_t s3c6410_touchscreen_read(conf_object_t *opaque, generic_address_t offset, void* buf, size_t count)
+
+static exception_t touchscreen_updata_status(conf_object_t* touchscreen)
 {
-	uint32_t ret;
-	struct s3c6410_fb_device *s = opaque->obj;
-	//DBG("yukewei ######################### In %s, offset=0x%x\n", __FUNCTION__, offset);
-	printf("yukewei ######################### In %s, offset=0x%x\n", __FUNCTION__, offset);
-	switch(offset) {
-	default:
-		printf("Can not read the register at 0x%x\n", offset);
-		return 0;
-	}
+	int *Pen_buffer = get_pen_buffer();
+	s3c6410_touchscreen_device* dev = (s3c6410_touchscreen_device*)touchscreen->obj;
+	touchscreen_reg_t* regs = dev->regs;
+	s3c6410_touchscreen_status* status = (s3c6410_touchscreen_status*)dev->status;
+#if 0
+	status->x = Pen_buffer[0];
+	status->y = Pen_buffer[1];
+	status->event = Pen_buffer[4];
+	status->stylus = Pen_buffer[5];
+	// set touchscreen status in regs
+	printf("x = %d, y = %d, envent = %d, stylus = %d\n",
+		status->x, status->y, status->event, status->stylus);
+#endif
+	return No_exp;
 }
 
-//static void s3c6410_fb_write(void *opaque, target_phys_addr_t offset,
- //                       uint32_t val)
+static exception_t s3c6410_touchscreen_read(conf_object_t *opaque, generic_address_t offset, void* buf, size_t count)
+{
+	struct s3c6410_touchscreen_device *dev = opaque->obj;
+	s3c6410_touchscreen_status* status = dev->status;
+	touchscreen_reg_t* regs = dev->regs;
+	DBG("yukewei ######################### In %s, offset=0x%x\n", __FUNCTION__, offset);
+	switch(offset) {
+		case 0x0:
+			*(uint32_t*)buf = regs->adccon; // 0x7E00_B000
+			break;
+		case 0x4:
+			*(uint32_t*)buf = regs->adctsc; // 0x7E00_B004
+			break;
+		case 0x8:
+			*(uint32_t*)buf = regs->adcdly; // 0x7E00_B008
+			break;
+		case 0x0C:
+			*(uint32_t*)buf = regs->adcdat0; // 0x7E00_B00C
+			break;
+		case 0x10:
+			*(uint32_t*)buf = regs->adcdat1; // 0x7E00_B010
+			break;
+		case 0x14:
+			*(uint32_t*)buf = regs->adcupdn; // 0x7E00_B014
+			break;
+		case 0x18:
+			*(uint32_t*)buf = regs->adcclrint; // 0x7E00_B018
+			break;
+		case 0x20:
+			*(uint32_t*)buf = regs->adcclrintpndnup; // 0x7E00_B020
+			break;
+		default:
+			printf("Can not read the register at 0x%x in touchscreen\n", offset);
+			return 0;
+	}
+	return No_exp;
+}
+
 static exception_t s3c6410_touchscreen_write(conf_object_t *opaque, generic_address_t offset, uint32_t* buf, size_t count)
 {
-	struct s3c6410_touchscreen_device *s = opaque->obj;
-	//DBG("yukewei ####################### In %s, offset=0x%x\n", __FUNCTION__, offset);
-	printf("yukewei ######################### In %s, offset=0x%x\n", __FUNCTION__, offset);
+	struct s3c6410_touchscreen_device *dev = opaque->obj;
+	s3c6410_touchscreen_status* status = dev->status;
+	touchscreen_reg_t* regs = dev->regs;
+	DBG("yukewei ######################### In %s, offset=0x%x\n", __FUNCTION__, offset);
 	uint32_t val = *(uint32_t*)buf;
 	switch(offset) {
-        default:
-            break;
+		case 0x0:
+			regs->adccon = val; // 0x7E00_B000
+			break;
+		case 0x4:
+			regs->adctsc = val; // 0x7E00_B004
+			break;
+		case 0x8:
+			regs->adcdly = val; // 0x7E00_B008
+			break;
+		case 0x0C:
+			regs->adcdat0 = val; // 0x7E00_B00C
+			break;
+		case 0x10:
+			regs->adcdat1 = val; // 0x7E00_B010
+			break;
+		case 0x14:
+			regs->adcupdn = val; // 0x7E00_B014
+			break;
+		case 0x18:
+			regs->adcclrint = val; // 0x7E00_B018
+			break;
+		case 0x20:
+			regs->adcclrintpndnup = val; // 0x7E00_B020
+			break;
+		default:
+			printf("Can not write the register at 0x%x in touchscreen\n", offset);
+			return 0;
 	}
+	return No_exp;
 }
 static conf_object_t* new_s3c6410_touchscreen(char* obj_name){
 	s3c6410_touchscreen_device* dev = skyeye_mm_zero(sizeof(s3c6410_touchscreen_device));
-	dev->obj = new_conf_object(obj_name, dev);
 	touchscreen_reg_t* regs =  skyeye_mm_zero(sizeof(touchscreen_reg_t));
+	s3c6410_touchscreen_status* status = skyeye_mm_zero(sizeof(s3c6410_touchscreen_status));
+	dev->obj = new_conf_object(obj_name, dev);
 	/* init touchscreen regs */
 	regs->adccon = 0x3fc4;
 	regs->adctsc = 0x0058;
 	regs->adcdly = 0x00ff;
 	regs->adcupdn = 0x0000;
 	dev->regs = regs;
+	dev->status = status;
+
+	int timer_id;
+	create_thread_scheduler(10000, Periodic_sched, touchscreen_updata_status, dev->obj, &timer_id);
 	/* Register io function to the object */
 	memory_space_intf* io_memory = skyeye_mm_zero(sizeof(memory_space_intf));
+	io_memory->conf_obj = dev->obj;
 	io_memory->read = s3c6410_touchscreen_read;
 	io_memory->write = s3c6410_touchscreen_write;
 	SKY_register_interface(io_memory, obj_name, MEMORY_SPACE_INTF_NAME);	
--- device/touchscreen_s3c6410/touchscreen_s3c6410.h
+++ device/touchscreen_s3c6410/touchscreen_s3c6410.h
@@ -1,18 +1,60 @@
 #ifndef __TOUCHSCREEN_S3C6410_H__
 #define __TOUCHSCREEN_S3C6410_H__
+typedef char BOOL;
+
 typedef struct touchscreen_reg{
-	uint32 adccon; // 0x7E00_B000
-	uint32 adctsc; // 0x7E00_B004
-	uint32 adcdly; // 0x7E00_B008
-	uint32 adcdat0; // 0x7E00_B00C
-	uint32 adcdat1; // 0x7E00_B010
-	uint32 adcupdn; // 0x7E00_B014
-	uint32 adcclrint; // 0x7E00_B018
-	uint32 adcclrintpndnup; // 0x7E00_B020
+	uint32_t adccon; // 0x7E00_B000
+	uint32_t adctsc; // 0x7E00_B004
+	uint32_t adcdly; // 0x7E00_B008
+	uint32_t adcdat0; // 0x7E00_B00C
+	uint32_t adcdat1; // 0x7E00_B010
+	uint32_t adcupdn; // 0x7E00_B014
+	uint32_t adcclrint; // 0x7E00_B018
+	uint32_t adcclrintpndnup; // 0x7E00_B020
 }touchscreen_reg_t; 
+
+typedef struct s3c6410_touchscreen_status{
+	int x;
+	int y;
+	int stylus; 	// down, up 
+	int event; 	// down, up 
+	int adcbit;	// 10 bit, 12 bit
+}s3c6410_touchscreen_status;
 
 typedef struct s3c6410_touchscreen_device{
 	conf_object_t* obj;
 	touchscreen_reg_t* regs;
+	s3c6410_touchscreen_status* status;
 }s3c6410_touchscreen_device;
+
+/* adccon mask */
+#define ENABLE_START	0x01 << 0
+#define READ_START	0x01 << 1
+#define STDBM		0x01 << 2
+#define SEL_MUX		0x07 << 3
+#define PRSCVL		0x7f << 6
+#define PRSCEN		0x01 << 14
+#define ECFLG		0x01 << 15
+#define RESSEL		0x01 << 16
+/* adctsc mask */
+#define TSC_XY_PST	0x03 << 0
+#define TSC_AUTO_PST	0x01 << 2
+#define PULL_UP		0x01 << 3
+#define XP_SEN		0x01 << 4
+#define XM_SEN		0x01 << 5
+#define YP_SEN		0x01 << 6
+#define YM_SEN		0x01 << 7
+#define UD_SEN		0x01 << 8
+/* adcdly mask*/
+
+/* adcdat0 adcdat1 mask */
+#define XY_PDATA_10		0x3ff << 0
+#define XY_PDATA_12		0xfff << 0
+#define DATA_XY_PST			0x003 << 12 
+#define DATA_AUTO_PST		0x001 << 14 
+#define UPDOWN			0x001 << 15 
+/* adcupdn mask */
+#define TSC_DN		0x1 << 1
+#define TSC_UP		0x1 << 0
+
 #endif
